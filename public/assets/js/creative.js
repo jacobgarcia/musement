2f418c65aebf11dc8bfc0b5d213ed4b04c066a45
Arturo Mendoza <arturo10.mr@gmail.com>
2016-02-06 20:24:38 -0600

    Font family ruler has been added, logo size in mobile now is more responsive

--- public/assets/js/creative.js
+++ public/assets/js/creative.js
@@ -27,13 +27,7 @@
         $('.navbar-toggle:visible').click();
     });
 
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
+
 
     // Offset for Main Navigation
     $('#mainNav').affix({
