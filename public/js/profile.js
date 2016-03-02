51591adcf040db58ada7106558706ececef05650
JacobGarcia <rock2494@hotmail.com>
2016-03-02 13:19:27 -0600

    Added routine for size management in profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -27,3 +27,14 @@ $(document).ready(function(){
         });
     }
 });
+
+function verifySize() {
+    const SIZELIMIT = 15728640; /* This is in bytes for 15 MB */
+    var size = document.getElementById('attachement').files[0].size;
+
+
+    if (size > SIZELIMIT) {
+      document.getElementById('fileError').text = "Tu imagen no puede ser mayor a 15 MB"
+      document.getElementById('attachement').value = null;
+    }
+}
