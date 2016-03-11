a01be0048aa25f31d18cb3d4d197a9f774f9339a
cesargdm <cesarguadarrama@icloud.com>
2016-03-11 14:05:38 -0600

    Delted unecessary script initaliation

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,11 +1,6 @@
-$(document).ready(function(){
-
-});
-
 function verifySize() {
     const SIZELIMIT = 15728640; /* This is in bytes for 15 MB */
     var size = document.getElementById('attachement').files[0].size;
-
 
     if (size > SIZELIMIT) {
       document.getElementById('fileError').text = "Tu imagen no puede ser mayor a 15 MB"
