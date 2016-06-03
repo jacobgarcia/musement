698657e8b05d0698e3fd2360f78017f86f8571bc
Arturo Mendoza <arturo10.mr@gmail.com>
2016-06-03 13:02:02 -0500

    Send Email is working but with the past version of the landing page

--- config/createinvitation.js
+++ config/createinvitation.js
@@ -1,4 +1,7 @@
 var Guest = require("models/guest.js");
+// var newEmail = require('config/sendEmail.js');
+const sendgrid  = require('sendgrid')('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');
+
 
 var insertInvite = function(req) {
   // Create invite
@@ -8,6 +11,27 @@ var insertInvite = function(req) {
   // newGuest.name = req.body.name;
   newGuest.email = req.body.email;
   // newGuest.whatismusement = req.body.whatismusement;
+
+  var email = new sendgrid.Email();
+  email.addTo(newGuest.email);
+  email.subject = "¡Bienvenido a Musement!";
+  email.from = 'hello@musement.co';
+  email.text = 'Bienvenido a Musement!';
+  email.html = '<h1>Bienvenido a Musement!</h1>';
+
+  // or set a filter using an object literal.
+  email.setFilters({
+      'templates': {
+          'settings': {
+              'enable': 1,
+              'template_id' : '9a114f42-5f35-4eef-87bb-144146b12645',
+          }
+      }
+  });
+  sendgrid.send(email, function(err, json) {
+    if (err) { console.error(err); }
+    console.log(json);
+  });
 
   console.log(newGuest.email);
 
@@ -20,6 +44,11 @@ var insertInvite = function(req) {
     }
   });
 }
+
+
+
+
+
 module.exports = {
   insertInvite: insertInvite
 }
