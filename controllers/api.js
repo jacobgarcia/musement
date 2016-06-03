1b0a9772a25d877698cc8f373fd3866350bfe7fd
cesargdm <cesarguadarrama@icloud.com>
2016-06-03 15:42:34 -0500

    Add

--- controllers/api.js
+++ controllers/api.js
@@ -52,7 +52,13 @@ router.post('/authenticate', function(req, res) {
   });
 });
 
-//MIDDLEWARE TOKEN-ACCESS
+
+// ********************************
+// **                            **
+// **  MIDDLEWARE TOKEN-ACCESS   **
+// **                            **
+// ********************************
+
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
