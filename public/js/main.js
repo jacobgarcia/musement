bc9e558bfc12f578f9722b640082db8b2ca98a0a
Arturo Mendoza <arturo10.mr@gmail.com>
2016-06-09 23:00:53 -0500

    Temporal fixed the second input mail

--- public/js/main.js
+++ public/js/main.js
@@ -25,6 +25,33 @@ $( "#submit-button" ).on("click",function() {
   });
 });
 
+$( "#submit-button2" ).on("click",function() {
+  $.getJSON("api/user/locale", function(userLocal) {
+    var locale = userLocal.locale;
+    var email = $("#email-input2").val();
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var isemail = regex.test(email);
+    if (isemail) {
+      var data = {"email": email};
+      $.post( "/api/invitation", data)
+      .done(function( data ) {
+        console.log("RESPONSE:");
+        console.dir(data);
+        $("#result").html( data );
+      });
+      if(locale === "es")
+          $('.thanks-email').text('Gracias, revisa tu correo :)');
+      else
+        $('.thanks-email').text('Thanks, please check your email :)');
+    }else{
+      if(locale === "es")
+          $('.thanks-email').text('Tu correo no es válido :(');
+      else
+        $('.thanks-email').text('Your email is not valid :(');
+    }
+  });
+});
+
 $('img.svg').each(function(){
     var $img = jQuery(this);
     var imgID = $img.attr('id');
