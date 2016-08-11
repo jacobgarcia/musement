5e24320678d55a19292bf7f7d26234fd06891f31
JacobGarcia <rock2494@hotmail.com>
2016-08-10 21:51:06 -0500

    Added ngMessages for message showing. Remember to bower install angular-messages

--- public/js/app.js
+++ public/js/app.js
@@ -49,4 +49,25 @@ angular.module('musementApp',['ui.router', 'LocalStorageModule', 'angular-jwt','
   //Decode token and asign info to user info div
   $scope.user = jwtHelper.decodeToken(localStorageService.get('token'));
   console.log("MAIN CTRL");
+})
+
+.directive('confirmPwd', function($interpolate, $parse) {
+  return {
+    require: 'ngModel',
+    link: function(scope, elem, attr, ngModelCtrl) {
+
+      var pwdToMatch = $parse(attr.confirmPwd);
+      var pwdFn = $interpolate(attr.confirmPwd)(scope);
+
+      scope.$watch(pwdFn, function(newVal) {
+          ngModelCtrl.$setValidity('password', ngModelCtrl.$viewValue == newVal);
+      })
+
+      ngModelCtrl.$validators.password = function(modelValue, viewValue) {
+        var value = modelValue || viewValue;
+        return value == pwdToMatch(scope);
+      };
+
+    }
+  }
 });
